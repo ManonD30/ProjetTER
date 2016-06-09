9342c6f13dea65141b1ce7cbea2d0eb08d44ea59
Vincent Emonet <vincent.emonet@gmail.com>
2016-06-09 11:03:00 +0200

    add test for yam

---
+++ src/test/java/YamTest.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import main.MainProgram;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author emonet
+ */
+public class YamTest {
+    
+    public YamTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+    
+    @Test
+    public void testYamMatch() {
+        /*MainProgram.match("/home/emonet/java_workspace/yam-gui/WebContent/data/cmt.owl",
+                            "/home/emonet/java_workspace/yam-gui/WebContent/data/Conference.owl",
+                            "/srv/yam-gui/result.rdf");*/
+    }
+}
