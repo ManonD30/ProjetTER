aad6195933efec4978530e3874bd7a81f1b185db
Vincent Emonet <vincent.emonet@gmail.com>
2016-07-13 20:30:55 +0200

    remove useless functions in download

--- src/main/java/fr/lirmm/opendata/yamgui/Download.java
+++ src/main/java/fr/lirmm/opendata/yamgui/Download.java
@@ -1,21 +1,12 @@
 package fr.lirmm.opendata.yamgui;
 
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.net.URI;
-import java.nio.channels.FileChannel;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Date;
 
 import javax.servlet.ServletException;
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -25,7 +16,6 @@ import org.semanticweb.owl.align.AlignmentVisitor;
 import fr.inrialpes.exmo.align.impl.BasicParameters;
 import fr.inrialpes.exmo.align.impl.URIAlignment;
 import fr.inrialpes.exmo.align.impl.renderer.RDFRendererVisitor;
-import java.util.Properties;
 
 public class Download extends HttpServlet {
 	private static final long serialVersionUID = 1L;
@@ -41,73 +31,33 @@ public class Download extends HttpServlet {
          */
 	public void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
+          // Force to get it as a file to download
+          response.setContentType("application/force-download");
+          response.setHeader("content-disposition", "inline; filename=\"yam_alignment_result.rdf\"");
 
-                // Load properties file for work directory
-                Properties prop = new Properties();
-                prop.load(Thread.currentThread().getContextClassLoader().getResourceAsStream("conf.properties"));
+          // request checked box
+          checked.clear();
+          String[] checkbox = request.getParameterValues("checkbox");
+          for (String c : checkbox) {
+                  int mapNumber = Integer.parseInt(c);
+                  checked.add(Result.liste.get(mapNumber));
+          }
 
-		// request checked box
-		checked.clear();
-		String[] checkbox = request.getParameterValues("checkbox");
-		for (String c : checkbox) {
-			int mapNumber = Integer.parseInt(c);
-			checked.add(Result.liste.get(mapNumber));
-		}
+          String alignmentString = generateAlignement(checked);
 
-		// create final file
-		String alignmentString = generateAlignement(checked);
-                
-                response.setCharacterEncoding("UTF-8");
-                response.setContentType("text/plain");
-                PrintWriter out = response.getWriter();
-                
-                out.print(alignmentString);
-                out.flush();
+          response.setCharacterEncoding("UTF-8");
+          PrintWriter out = response.getWriter();
+
+          out.print(alignmentString);
+          out.flush();
 	}
 
-	// save final rdf file
-	public void saveRDFFile(String key) throws IOException {
-		// current date to create a unique name
-		// rdf+date.rdf
-		DateFormat dateFormat = new SimpleDateFormat("yyyyMMddHHmmss");
-		Date date = new Date();
-		String dateName = dateFormat.format(date);
-
-                // Load properties file for work directory
-                Properties prop = new Properties();
-                prop.load(Thread.currentThread().getContextClassLoader().getResourceAsStream("conf.properties"));
-                
-		FileChannel in = null; // input
-		FileChannel out = null; // output
-
-		try {
-			// Init
-
-			in = new FileInputStream(prop.getProperty("workdir") + "/ontologies/finalResult" + key
-					+ ".rdf").getChannel();
-			out = new FileOutputStream(prop.getProperty("workdir") + "/save/" + dateName + "_"
-					+ key + "rdf.rdf").getChannel();
-
-			// Copy from in to out
-			in.transferTo(0, in.size(), out);
-		} catch (Exception e) {
-			e.printStackTrace();
-		} finally {
-			if (in != null) {
-				try {
-					in.close();
-				} catch (IOException e) {
-				}
-			}
-			if (out != null) {
-				try {
-					out.close();
-				} catch (IOException e) {
-				}
-			}
-		}
-	}
-
+	/**
+         * Generate alignment String retrieved from validation UI to generate the 
+         * alignment in RDF/XML format
+         * @param MapFinal
+         * @return 
+         */
 	public static String generateAlignement(ArrayList<Map> MapFinal) {
 
 		Alignment alignments = new URIAlignment();
