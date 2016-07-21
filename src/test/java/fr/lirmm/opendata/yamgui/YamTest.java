cd7609e83d4ed0d3b2826ec2a450289243db8571
Vincent Emonet <vincent.emonet@gmail.com>
2016-07-21 18:52:39 +0200

    edit conf.properties and add a little test

--- src/test/java/fr/lirmm/opendata/yamgui/YamTest.java
+++ src/test/java/fr/lirmm/opendata/yamgui/YamTest.java
@@ -1,10 +1,15 @@
+package fr.lirmm.opendata.yamgui;
+
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 //import main.MainProgram;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -43,10 +48,14 @@ public class YamTest {
   // @Test
   // public void hello() {}
   @Test
-  public void testYamMatch() {
+  public void testYamMatch() throws IOException, ClassNotFoundException {
     // Fail a cause de net.didion.jwnl.jwnl exception : WordNet. Passer direct à 2013
     /*MainProgram.match("/home/emonet/java_workspace/yam-gui/WebContent/data/cmt.owl",
                             "/home/emonet/java_workspace/yam-gui/WebContent/data/Conference.owl",
                             "/srv/yam-gui/result.rdf");*/
+    YamFileHandler fileHandler = new YamFileHandler();
+    System.out.println("print in test");
+    
+    assertEquals("/srv/yam-gui", fileHandler.getWorkDir());
   }
 }
