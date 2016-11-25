357fe4eddf334b17aab23a00469c0e9384b24faf
Vincent Emonet <vincent.emonet@gmail.com>
2016-11-25 22:09:19 +0100

    remove unused functions

--- src/main/webapp/scripts/javascript.js
+++ src/main/webapp/scripts/javascript.js
@@ -16,35 +16,3 @@ function refreshFileUpload(uploadFilename, labelToUpdate) {
   var fileName = path.match(/[^\/\\]+$/);
   $('#' + labelToUpdate).html(fileName);
 }
-
-// display download button if at least one checkbox is checked
-function seeDownloadButton() {
-
-  var atLeastOneChecked = false;
-  var i = 0;
-
-  while (document.getElementById(i)) {
-    if (document.getElementById(i).checked) {
-      atLeastOneChecked = true;
-      break;
-    }
-    i++;
-  }
-
-  if (atLeastOneChecked == true) {
-    hide_div('toHideWhenDownload');
-    display_div('seeMappings');
-    display_div('download');
-  } else {
-    alert("Please check at least one alignment.");
-    return false;
-  }
-}
-
-// display mappings tab and hide download button
-function seeMappings() {
-  hide_div('download');
-  hide_div('seeMappings');
-  display_div('toHideWhenDownload');
-}
-
