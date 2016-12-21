fe83149a3c6446950a3d6881f48bed68661cc619
Vincent Emonet <vincent.emonet@gmail.com>
2016-12-21 23:18:36 +0100

    use -i or --install with compile.sh to instal local dependencies

--- compile.sh
+++ compile.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
+
+if [ "$1" == "-i" ] || [ "$1" == "--install" ]
+then
+  ./maven_install_local_dependencies.sh
+fi
+
 mvn clean package -Dmaven.test.skip=true
 
-# For Docker tomcat:
+# Upload to Docker yam_tomcat:
 docker cp target/yam.war yam_tomcat:/usr/local/tomcat/webapps/ROOT.war
