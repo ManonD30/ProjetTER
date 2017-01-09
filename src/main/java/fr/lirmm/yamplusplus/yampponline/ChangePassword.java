7dbdddbcb7977c88cd2c69db321539fecd814c1a
Vincent Emonet <vincent.emonet@gmail.com>
2017-01-10 00:58:27 +0100

    reformate change password

--- src/main/java/fr/lirmm/yamplusplus/yampponline/ChangePassword.java
+++ src/main/java/fr/lirmm/yamplusplus/yampponline/ChangePassword.java
@@ -13,115 +13,117 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class ChangePassword extends HttpServlet {
-	private static final long serialVersionUID = 1L;
 
-	public void doPost(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
+  private static final long serialVersionUID = 1L;
 
-		String mail = (String) request.getSession().getAttribute("mail");
-		String oldPass = request.getParameter("oldPassword");
-		String newPass = request.getParameter("newPassword");
-		String user = null;
+  public void doPost(HttpServletRequest request, HttpServletResponse response)
+          throws ServletException, IOException {
 
-		try {
-			// create a mysql database connection
-			String myDriver = "org.gjt.mm.mysql.Driver";
-			String myUrl = "jdbc:mysql://localhost/yam";
-			Class.forName(myDriver);
-			Connection conn = DriverManager.getConnection(myUrl, "root",
-					"lirmmpass");
+    String mail = (String) request.getSession().getAttribute("mail");
+    String oldPass = request.getParameter("oldPassword");
+    String newPass = request.getParameter("newPassword");
+    String user = null;
 
-			// mysql request
-			String query =  "SELECT name FROM user WHERE mail= ? AND password = ?";
+    try {
+      // create a mysql database connection
+      String myDriver = "org.gjt.mm.mysql.Driver";
+      String myUrl = "jdbc:mysql://localhost/yam";
+      Class.forName(myDriver);
+      Connection conn = DriverManager.getConnection(myUrl, "root",
+              "lirmmpass");
 
-			// create the mysql prepared statement
-			PreparedStatement preparedStmt = conn.prepareStatement(query);
-			preparedStmt.setString(1, mail);
-			preparedStmt.setString(2, hash(oldPass));
-			
-			// execute the prepared statement
-			ResultSet result = preparedStmt.executeQuery();
-			while (result.next()) {
-				user = result.getString("name");
-			}
-			// close connection to database
-			conn.close();
+      // mysql request
+      String query = "SELECT name FROM user WHERE mail= ? AND password = ?";
 
-		} catch (Exception e) {
-			System.err.println("Exception catched!");
-			System.err.println(e.getMessage());
-		}
+      // create the mysql prepared statement
+      PreparedStatement preparedStmt = conn.prepareStatement(query);
+      preparedStmt.setString(1, mail);
+      preparedStmt.setString(2, hash(oldPass));
 
-		// if mail / password not in DB
-		if (user == null) {
-			// error message
-			request.setAttribute("error",
-					"You entered an invalid old password.");
-			// send response
-			this.getServletContext()
-					.getRequestDispatcher("/WEB-INF/change.jsp")
-					.forward(request, response);
+      // execute the prepared statement
+      ResultSet result = preparedStmt.executeQuery();
+      while (result.next()) {
+        user = result.getString("name");
+      }
+      // close connection to database
+      conn.close();
 
-		} else {
-			// change password
-			try {
-				// create a mysql database connection
-				String myDriver = "org.gjt.mm.mysql.Driver";
-				String myUrl = "jdbc:mysql://localhost/yam";
-				Class.forName(myDriver);
-				Connection conn = DriverManager.getConnection(myUrl, "root",
-						"lirmmpass");
+    } catch (Exception e) {
+      System.err.println("Exception catched!");
+      System.err.println(e.getMessage());
+    }
 
-				// mysql request
-				String query = "UPDATE user SET password=? WHERE mail=?";
-				// create the mysql prepared statement
-				PreparedStatement preparedStmt = conn.prepareStatement(query);
-				preparedStmt.setString(1, hash(newPass));
-				preparedStmt.setString(2, mail);
-				// execute the prepared statement
-				preparedStmt.executeUpdate();
-				// close connection to database
-				conn.close();
-			} catch (Exception e) {
-				System.err.println("Exception catched!");
-				System.err.println(e.getMessage());
-			}
+    // if mail / password not in DB
+    if (user == null) {
+      // error message
+      request.setAttribute("error",
+              "You entered an invalid old password.");
+      // send response
+      this.getServletContext()
+              .getRequestDispatcher("/WEB-INF/change.jsp")
+              .forward(request, response);
 
-			request.setAttribute("error", "Password successfully changed.");
-			// send response
-			this.getServletContext()
-					.getRequestDispatcher("/WEB-INF/change.jsp")
-					.forward(request, response);
-		}
-	}
+    } else {
+      // change password
+      try {
+        // create a mysql database connection
+        String myDriver = "org.gjt.mm.mysql.Driver";
+        String myUrl = "jdbc:mysql://localhost/yam";
+        Class.forName(myDriver);
+        Connection conn = DriverManager.getConnection(myUrl, "root",
+                "lirmmpass");
 
-	public void doGet(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
+        // mysql request
+        String query = "UPDATE user SET password=? WHERE mail=?";
+        // create the mysql prepared statement
+        PreparedStatement preparedStmt = conn.prepareStatement(query);
+        preparedStmt.setString(1, hash(newPass));
+        preparedStmt.setString(2, mail);
+        // execute the prepared statement
+        preparedStmt.executeUpdate();
+        // close connection to database
+        conn.close();
+      } catch (Exception e) {
+        System.err.println("Exception catched!");
+        System.err.println(e.getMessage());
+      }
 
-		request.setAttribute("error", "");
-		this.getServletContext().getRequestDispatcher("/WEB-INF/change.jsp")
-				.forward(request, response);
-	}
+      request.setAttribute("error", "Password successfully changed.");
+      // send response
+      this.getServletContext()
+              .getRequestDispatcher("/WEB-INF/change.jsp")
+              .forward(request, response);
+    }
+  }
 
-	// method which hash String with prefix
-	// prefix have to be the same when user is registering or connecting
-	public String hash(String password) {
-		try {
-			password = password + "WONh31K5RYaal07";
-			MessageDigest digest = MessageDigest.getInstance("SHA-256");
-			byte[] hash = digest.digest(password.getBytes("UTF-8"));
-			StringBuffer hexString = new StringBuffer();
+  public void doGet(HttpServletRequest request, HttpServletResponse response)
+          throws ServletException, IOException {
 
-			for (int i = 0; i < hash.length; i++) {
-				String hex = Integer.toHexString(0xff & hash[i]);
-				if (hex.length() == 1)
-					hexString.append('0');
-				hexString.append(hex);
-			}
+    request.setAttribute("error", "");
+    this.getServletContext().getRequestDispatcher("/WEB-INF/change.jsp")
+            .forward(request, response);
+  }
 
-			return hexString.toString();
-		} catch (Exception ex) {
-			throw new RuntimeException(ex);
-		}
-	}
+  // method which hash String with prefix
+  // prefix have to be the same when user is registering or connecting
+  public String hash(String password) {
+    try {
+      password = password + "WONh31K5RYaal07";
+      MessageDigest digest = MessageDigest.getInstance("SHA-256");
+      byte[] hash = digest.digest(password.getBytes("UTF-8"));
+      StringBuffer hexString = new StringBuffer();
+
+      for (int i = 0; i < hash.length; i++) {
+        String hex = Integer.toHexString(0xff & hash[i]);
+        if (hex.length() == 1) {
+          hexString.append('0');
+        }
+        hexString.append(hex);
+      }
+
+      return hexString.toString();
+    } catch (Exception ex) {
+      throw new RuntimeException(ex);
+    }
+  }
 }
